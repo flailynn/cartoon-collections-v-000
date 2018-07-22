f4cd934a4744ca81faa750ec4978ae3b727c4076
Anita Beauchamp <amadamspr@gmail.com>
2018-07-22 19:46:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,8 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)
+  calls.any? {|call| call.size > 4}
   # Your code here
 end
 
